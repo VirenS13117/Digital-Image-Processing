7ca64f6c77db9a5fdb1975032265d5284bbd506b
Virender Singh <virender13117@gmail.com>
2018-10-24 16:28:25 +0530

    Image Addition Operation

---
+++ Basic_Image_Operations/Debug/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lopencv_core -lopencv_legacy -lopencv_contrib -lopencv_objdetect -lopencv_calib3d -lopencv_features2d -lopencv_video -lopencv_ml -lopencv_highgui -lopencv_flann -lopencv_imgproc
+
---
+++ Basic_Image_Operations/Debug/src/add.d
@@ -0,0 +1,6 @@
+src/add.d: ../src/add.cpp /usr/include/opencv/cv.h \
+ /usr/include/opencv/highgui.h
+
+/usr/include/opencv/cv.h:
+
+/usr/include/opencv/highgui.h:
